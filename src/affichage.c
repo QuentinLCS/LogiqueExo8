fd4f252e9567366d13f15fc95455fc31325a2294
Quentin LECHASLES <quentin.lechasles@gmail.com>
2021-02-28 19:54:51 +0100

    Systeme de sauvegarde + lecture des résultats du solver

--- src/affichage.c
+++ src/affichage.c
@@ -10,7 +10,11 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "../include/struct.h"
+#include "../include/coloriage.h"
+
 
 /**
  * @brief Affiche un tableau d'ARETES.
@@ -23,7 +27,7 @@ void afficherAretes(int size, ARETE* tableau) {
     for (int i = 0; i < size; i++)
     {
     
-        printf("[%d] >> (%c) %d ---- %d (%c)\n", i+1, tableau[i].premierSommet.couleur, tableau[i].premierSommet.id, tableau[i].secondSommet.id, tableau[i].premierSommet.couleur);
+        printf("[%d] >> %d ---- %d\n", i+1, tableau[i].premierSommet.id, tableau[i].secondSommet.id);
         
     }
     
@@ -41,59 +45,111 @@ void afficherAretesDimacs(int nbSommets, int nbAretes, ARETE* tableau) {
 
     printf("\n\n-----------------  DIMACS FORMAT  -----------------\n\n");
     
-    int nbVariables = nbSommets * 3;
-    int nbClauses = nbSommets + nbSommets * 3 + nbAretes * 3;
-    printf("p cnf %d %d\n", nbVariables, nbClauses);
-    
-    for (int i = 1; i <= nbSommets; i++)
-    {
-    
-        int Si = i * 3;
-        // 1) Chaque sommet i = 1, . . . n prend au moins une couleur.
-        printf("%d %d %d 0\n", Si, Si-1, Si-2);
+    for (int save = 0; save < 2; save++) {
 
-        // 2) Chaque sommet i = 1, . . . prend au plus une couleur 
-        // (si le sommet est en vert alors il n’est pas en rouge, etc).
-        printf("%d %d 0\n", -Si, -Si+1);
-        printf("%d %d 0\n", -Si, -Si+2);
-        printf("%d %d 0\n", -Si+1, -Si+2);
+        char resultat[10000] = "";
+        char tempString[200] = "";
+        int nbVariables = nbSommets * 3;
+        int nbClauses = nbSommets + nbSommets * 3 + nbAretes * 3;
+        sprintf(resultat, "p cnf %d %d\n", nbVariables, nbClauses);
         
-    }
+        if (save) {
 
-    for (int i = 0; i < nbAretes; i++) {
+            sprintf(tempString, "a 0\ne ");
+            strcat(resultat, tempString);
 
-        for (int j = 1; j <= 3; j++) {
+            for (int i = 1; i <= nbSommets*3; i++) {
+                sprintf(tempString, "%d ", i);
+                strcat(resultat, tempString);
+            }
 
-            // Les deux sommets d’une arˆete (Si, Sj) ne prennent pas la même
-            // couleur c pour 1 ≤ i < j ≤ n, pour c ∈ {v,r, b}.
-            printf("%d %d 0\n", -((tableau[i].premierSommet.id-1)*3 + j), -((tableau[i].secondSommet.id-1)*3 + j));
+            sprintf(tempString, "0\n");
+            strcat(resultat, tempString);
 
         }
+        
+
+        for (int i = 1; i <= nbSommets; i++)
+        {
+            int Si = i * 3;
+            // 1) Chaque sommet i = 1, . . . n prend au moins une couleur.
+            sprintf(tempString, "%d %d %d 0\n", Si, Si-1, Si-2);
+            strcat(resultat, tempString);
+
+
+            // 2) Chaque sommet i = 1, . . . prend au plus une couleur 
+            // (si le sommet est en vert alors il n’est pas en rouge, etc).
+            sprintf(tempString, "%d %d 0\n", -Si, -Si+1);
+            strcat(resultat, tempString);
+
+            sprintf(tempString, "%d %d 0\n", -Si, -Si+2);
+            strcat(resultat, tempString);
+
+            sprintf(tempString, "%d %d 0\n", -Si+1, -Si+2);
+            strcat(resultat, tempString);
             
+        }
+
+        for (int i = 0; i < nbAretes; i++) {
+
+            for (int j = 1; j <= 3; j++) {
+
+                // Les deux sommets d’une arˆete (Si, Sj) ne prennent pas la même
+                // couleur c pour 1 ≤ i < j ≤ n, pour c ∈ {v,r, b}.
+                sprintf(tempString, "%d %d 0\n", -((tableau[i].premierSommet.id-1)*3 + j), -((tableau[i].secondSommet.id-1)*3 + j));
+                strcat(resultat, tempString);
+
+            }
+                
+        }
+
+        if (save) sauvegarderDansUnFichier(resultat);
+        else printf("%s", resultat);
     }
+    
 
     printf("\n----------------------  FIN  ----------------------\n\n");
-    
+
 }
 
+/**
+ * @brief Affiche les erreurs en fonction de l'entier donné en paramètre.
+ * 
+ * @param valeur Code d'erreur. ( 1 ou 2 )
+ */ 
 void erreurFonctionnelle(int valeur) {
 
-    fprintf(stderr, "\n--------------------------\n");
+    char erreur[1000] = "\n--------------------------\n";
 
     switch (valeur)
     {
     case 1:
-        fprintf(stderr, "Nombre d'arguments insuffisant. \nERREUR : Minimum 5 et le nombre de paramètres doit être pair.");
+        strcat(erreur, "Nombre d'arguments insuffisant. \nERREUR : Minimum 5 et le nombre de paramètres doit être pair.");
         break;
 
     case 2:
-        fprintf(stderr, "Nombre d'arguments incohérent. \nERREUR : les 2 extrêmités des aretes doivent être renseignées.");
+        strcat(erreur, "Nombre d'arguments incohérent. \nERREUR : les 2 extrêmités des aretes doivent être renseignées.");
         break;
-    
+
+    case 3:
+        strcat(erreur, "Sauvegarde du résultat impossible. \nERREUR : Vérifiez que le dossier \"./saves\" dispose des droits requis à l'écriture ainsi qu'à la lecture d'un fichier format DIMACS.");
+        break;
+
+    case 4:
+        strcat(erreur, "Execution du solver impossible. \nERREUR : vérifiez que vous disposez du dossier solver et que ses droits sont suffisants afin que le programme puisse utiliser un executable.");
+        break;
+
+    case 5:
+        break;
+
     default:
+        strcat(erreur, "Lecture du solver impossible. \nERREUR : Vérifiez que le fichier .\"/solver/result.aig\" existe et que les droits requis sont définis.");
         break;
     }
 
-    fprintf(stderr, "\n--------------------------\n");
+    strcat(erreur, "\n--------------------------\n");
+    
+    fprintf(stderr, "%s", erreur);
 
 }
+
---
+++ src/coloriage.c
@@ -0,0 +1,97 @@
+/**
+ * @file coloriage.c
+ * @author Quentin LECHASLES (contact@quentinlcs.fr)
+ * @brief Fonctions permettant de déterminer si un graphe est coloriable en 3 couleurs ou non.
+ * @version 0.1
+ * @date 2021-02-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/struct.h"
+#include "../include/affichage.h"
+
+/**
+ * @brief Ecrit la formule DIMACS dans un fichier.
+ * 
+ * @param chaineDeCaracteres Caractères à écrire dans le fichier de sauvegarde.
+ */
+void sauvegarderDansUnFichier(char* chaineDeCaracteres) {
+
+    FILE* fichier = NULL;
+
+    fichier = fopen( "./saves/last-save.txt", "r+" );
+
+    system(">./saves/last-save.txt");
+
+    if (fichier != NULL)
+    {
+        
+        fseek(fichier, 0, SEEK_END);
+
+        fprintf(fichier, "%s", chaineDeCaracteres);
+
+        fclose(fichier);
+    }
+    else
+    {
+        erreurFonctionnelle(3);
+    }
+}
+
+/**
+ * @brief Execute un code openSource à l'aide des informations précédemment obtenues et donne le coloriage du graphe s'il est possible.
+ * 
+ */
+void colorierLeGraphe(int nbSommets) {
+
+    int curseur = 14, tempInt = 0;
+    char couleurs[3] = {ROUGE, VERT, BLEU};
+
+    if (system("./solver/cadet -c result.aig ./saves/last-save.txt")) {
+
+        FILE* fichier = NULL;
+
+        fichier = fopen( "./solver/result.aig", "r+" );
+        fseek(fichier, curseur, SEEK_SET);
+
+        if (fichier != NULL)
+        {
+            printf("\n---------  RESULTATS  ---------\n\n");
+            for (int sommetNum = 1; sommetNum <= nbSommets; sommetNum++) {
+                for (int colorNum= 0; colorNum < 3; colorNum++)
+                {
+
+                    fscanf(fichier, "%d", &tempInt);
+
+                    if (tempInt) printf("(%d) : %c\n", sommetNum, couleurs[colorNum]);
+                    
+                }
+            }
+
+            fscanf(fichier, "%d", &tempInt);
+            if (tempInt) printf("SAT - LE GRAPHE EST COLORIABLE EN 3 COULEURS !");
+            else if (tempInt) printf("UNSAT - GRAPHE NON COLORABLE EN 3 COULEURS...");
+        
+            fclose(fichier);
+            
+        }
+        else {
+
+            erreurFonctionnelle(5);
+
+        }
+        
+
+    } else {
+
+        erreurFonctionnelle(4);
+
+    }
+
+    printf("\n\n\n");
+
+}
